49dabd141ce5965503200cf9f5f70e157ab09dca
Diogoo <diogo.novodamata@gmail.com>
2024-07-04 10:25:07 +0200

    add more styling

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -1,21 +1,34 @@
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.*
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.graphics.Shadow
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import kotlinx.coroutines.CoroutineScope
-import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 import util.Gelds
 import util.toHumanReadableString
@@ -33,13 +46,6 @@ fun App() {
 @Preview
 fun Screen() {
     Scaffold(
-        topBar = {
-            TopAppBar(
-                title = { Text("Dragon Ball Universe Master") },
-                backgroundColor = Color(0xFF6200EA),
-                contentColor = Color.White
-            )
-        },
         content = {
             val coroutineScope = rememberCoroutineScope()
             val viewModel by remember {
@@ -61,55 +67,64 @@ fun Screen() {
             }
 
             Column(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(16.dp)
+                modifier = Modifier.fillMaxWidth()
                     .verticalScroll(rememberScrollState())
+                    .padding(
+                        horizontal = 100.dp,
+                    ),
+                horizontalAlignment = Alignment.End
             ) {
-                Spacer(modifier = Modifier.height(16.dp))
-                Button(
-                    onClick = { viewModel.reset() },
-                    colors = ButtonDefaults.buttonColors(backgroundColor = Color.Red),
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(8.dp)
-                ) {
-                    Text("Reset Game", color = Color.White, fontSize = 18.sp)
-                }
+
+            }
+
+
+            Column(
+                modifier = Modifier.fillMaxWidth()
+                    .verticalScroll(rememberScrollState())
+
+            ) {
+
+
+
+                Text(
+                    text = buildAnnotatedString {
+                        withStyle(style = SpanStyle(fontWeight = FontWeight.Bold, fontSize = 48.sp, shadow = Shadow(color = Color.Gray, offset = Offset(2f, 2f), blurRadius = 4f))) {
+                            append("DRAGON ")
+                        }
+                        withStyle(style = SpanStyle(fontWeight = FontWeight.Bold, fontSize = 48.sp, color = Color.Red, shadow = Shadow(color = Color.Gray, offset = Offset(2f, 2f), blurRadius = 4f))) {
+                            append("BALL")
+                        }
+                        append("\n")
+                        withStyle(style = SpanStyle(fontWeight = FontWeight.Bold, fontSize = 48.sp, shadow = Shadow(color = Color.Gray, offset = Offset(2f, 2f), blurRadius = 4f))) {
+                            append("UNIVERSE MASTER")
+                        }
+                    },
+                    style = MaterialTheme.typography.h1,
+                )
+
+
+                Spacer(modifier = Modifier.height(100.dp))
+
+
 
                 gameState?.let { state ->
                     Text(
                         "Bank: ${currentMoney?.toHumanReadableString()} Gelds",
-                        style = MaterialTheme.typography.h4.copy(fontWeight = FontWeight.Bold),
-                        color = Color(0xFF6200EA),
-                        modifier = Modifier.padding(8.dp)
+                        style = MaterialTheme.typography.h4,
                     )
-
                     Button(
-                        onClick = { viewModel.clickMoney(state) },
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(8.dp)
-                            .height(100.dp) // Set height as needed
-                            .background(Color.Transparent) // Transparent background for button
+                        onClick = { viewModel.clickMoney(state) }
                     ) {
-                        Image(
-                            painter = painterResource(), // Use the actual image resource name
-                            contentDescription = "Click Money",
-                            contentScale = ContentScale.Crop,
-                            modifier = Modifier.fillMaxSize()
-                        )
+                        Text("Click To Sell Lemonade")
                     }
 
                     state.availableJobs.forEach { availableJob ->
                         Generator(
+
                             gameJob = availableJob,
                             alreadyBought = state.workers.any { it.jobId == availableJob.id },
                             onBuy = { viewModel.addWorker(state, availableJob) },
-                            onUpgrade = { viewModel.upgradeJob(state, availableJob) },
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .padding(8.dp)
+                            onUpgrade = { viewModel.upgradeJob(state, availableJob) }
                         )
                     }
                 }
@@ -126,38 +141,31 @@ private fun Generator(
     onBuy: () -> Unit = {},
     onUpgrade: () -> Unit = {},
 ) {
-    Card(
-        shape = RoundedCornerShape(8.dp),
-        backgroundColor = Color(0xFFF5F5F5),
-        elevation = 4.dp,
+    Row(
+        horizontalArrangement = Arrangement.spacedBy(16.dp),
         modifier = modifier
+            .padding(8.dp)
+            .background(Color.Red, RoundedCornerShape(8.dp))
+            .padding(8.dp)
     ) {
-        Row(
-            horizontalArrangement = Arrangement.SpaceBetween,
-            verticalAlignment = Alignment.CenterVertically,
-            modifier = Modifier
-                .padding(16.dp)
-        ) {
-            Column {
-                Text("Generator ${gameJob.id}", fontWeight = FontWeight.Bold)
-                Text("Level: ${gameJob.level.level}")
-                Text("Costs: ${gameJob.level.cost.toHumanReadableString()} Gelds")
-                Text("Earns: ${gameJob.level.earn.toHumanReadableString()} Gelds")
-                Text("Duration: ${gameJob.level.duration.inWholeSeconds} Seconds")
+        Column {
+            Text("Generator ${gameJob.id}")
+            Text("Level: ${gameJob.level.level}")
+            Text("Costs: ${gameJob.level.cost.toHumanReadableString()} Gelds")
+            Text("Earns: ${gameJob.level.earn.toHumanReadableString()} Gelds")
+            Text("Duration: ${gameJob.level.duration.inWholeSeconds} Seconds")
+        }
+
+        if (!alreadyBought) {
+            Button(onClick = onBuy) {
+                Text("Buy")
+
             }
-            Column {
-                if (!alreadyBought) {
-                    Button(onClick = onBuy) {
-                        Text("Buy")
-                    }
-                } else {
-                    Text("Bought", color = Color.Green, fontWeight = FontWeight.Bold)
-                }
-                Spacer(modifier = Modifier.height(8.dp))
-                Button(onClick = onUpgrade) {
-                    Text("Upgrade")
-                }
-            }
+        } else {
+            Text("Bought")
+        }
+        Button(onClick = onUpgrade) {
+            Text("Upgrade")
         }
     }
 }
