db627444e981466f8e6cb924d92bb67a648ae93f
Diogoo <diogo.novodamata@gmail.com>
2024-07-09 11:54:01 +0200

    mehr buttons zum upgraden

--- composeApp/src/commonMain/kotlin/Game.kt
+++ composeApp/src/commonMain/kotlin/Game.kt
@@ -14,12 +14,12 @@ data class GameState(
     val workers: List<GameWorker>,
     val availableJobs: List<GameJob> = listOf(
         // level, kosten, einkommen, dauer
-        GameJob(1, Level(1, 10.gelds, 1.gelds, 2.seconds)),
-        GameJob(2, Level(1, 200.gelds, 9.gelds, 3.seconds)),
-        GameJob(3, Level(1, 2000.gelds, 800.gelds, 10.seconds)),
-        GameJob(4, Level(1, 80000.gelds, 3000.gelds, 15.seconds)),
-        GameJob(5, Level(1, 2000000.gelds, 9000.gelds, 20.seconds)),
-        GameJob(6, Level(1, 50000000.gelds, 1000000.gelds, 30.seconds))
+        GameJob(1, Level(1, 50.gelds, 5.gelds, 2.seconds)),
+        GameJob(2, Level(1, 300.gelds, 10.gelds, 3.seconds)),
+        GameJob(3, Level(1, 2000.gelds, 150.gelds, 10.seconds)),
+        GameJob(4, Level(1, 10000.gelds, 800.gelds, 15.seconds)),
+        GameJob(5, Level(1, 100000.gelds, 1000.gelds, 20.seconds)),
+        GameJob(6, Level(1, 500000.gelds, 250000.gelds, 30.seconds))
 
 
     )
